cc1fbe077fbb36d420b3e268ce3cee7e7ccbb2d9
NemeanLion42 <jack@tinyx.com>
2023-02-21 04:56:32 +0000

    Create Custom Message

--- src/open_space_publisher.py
+++ src/open_space_publisher.py
@@ -3,17 +3,17 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32
+from ros_exercises.msg import OpenSpace
 
 def open_space_publisher():
-    pub_distance = rospy.Publisher('open_space/distance', Float32, queue_size=10)
-    pub_angle = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+    pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
     rospy.init_node('open_space_publisher')
     def callback(scan):
         max_range = max(scan.ranges)
         index_max = scan.ranges.index(max_range)
         angle = scan.angle_min + index_max*scan.angle_increment
-        pub_distance.publish(max_range)
-        pub_angle.publish(angle)
+        space = OpenSpace(angle=angle, distance=max_range)
+        pub.publish(space)
     rospy.Subscriber('fake_scan', LaserScan, callback)
     rospy.spin()
 
