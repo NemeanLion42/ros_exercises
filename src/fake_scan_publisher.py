81b3cca61900787e1788613d88b6191bc04bf32c
NemeanLion42 <jack@tinyx.com>
2023-02-21 16:01:28 +0000

    Use ROS Parameters

--- src/fake_scan_publisher.py
+++ src/fake_scan_publisher.py
@@ -7,19 +7,21 @@ import math
 import random
 
 def fake_scan_publisher():
-    pub = rospy.Publisher('fake_scan', LaserScan, queue_size=10)
     rospy.init_node('fake_scan_publisher')
-    rate = rospy.Rate(20)
+    topic = rospy.get_param("topic", "fake_scan")
+    rate_num = rospy.get_param("rate", 20)
+    rate = rospy.Rate(rate_num)
+    scan_time = 1/rate_num
+    angle_min = rospy.get_param("angle_min", math.pi*-2/3)
+    angle_max = rospy.get_param("angle_max", math.pi*2/3)
+    angle_increment = rospy.get_param("angle_increment", math.pi/300)
+    range_min = rospy.get_param("range_min", 1)
+    range_max = rospy.get_param("range_max", 10)
+    pub = rospy.Publisher(topic, LaserScan, queue_size=10)
 
     while not rospy.is_shutdown():
         time = rospy.get_rostime()
         frame = "base_link"
-        angle_min = math.pi*-2/3
-        angle_max = math.pi*2/3
-        angle_increment = math.pi/300
-        scan_time = 1/20
-        range_min = 1
-        range_max = 10
         ranges = [random.random()*(range_max-range_min) + range_min for e in range(0, int(round((angle_max-angle_min)/angle_increment) + 1))]
         scan = LaserScan(
             header=Header(
--- src/open_space_publisher.py
+++ src/open_space_publisher.py
@@ -6,15 +6,17 @@ from std_msgs.msg import Float32
 from ros_exercises.msg import OpenSpace
 
 def open_space_publisher():
-    pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
     rospy.init_node('open_space_publisher')
+    subscriber_topic = rospy.get_param("subscriber_topic", "fake_scan")
+    publisher_topic = rospy.get_param("publisher_topic", "open_space")
+    pub = rospy.Publisher(publisher_topic, OpenSpace, queue_size=10)
     def callback(scan):
         max_range = max(scan.ranges)
         index_max = scan.ranges.index(max_range)
         angle = scan.angle_min + index_max*scan.angle_increment
         space = OpenSpace(angle=angle, distance=max_range)
         pub.publish(space)
-    rospy.Subscriber('fake_scan', LaserScan, callback)
+    rospy.Subscriber(subscriber_topic, LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
